2bea97e8fe5477b5fc9d0d19a64585cea5a171bb
alifahsanul <alifahsanul@gmail.com>
2025-04-28 12:28:10 +0900

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -55,4 +55,4 @@ if __name__ == "__main__":
     import uvicorn
 
     port = int(os.environ.get("PORT", 8000))  # Read from env, default to 8000
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
